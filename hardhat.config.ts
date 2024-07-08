79969a7a1541dc72f9dc916f8d83eab8edc36a29
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-07-08 21:47:38 +0200

    Add Optimism support in Hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const ACCOUNT_PRIVATE_KEY = vars.get("ACCOUNT_PRIVATE_KEY")
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    optimism: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
